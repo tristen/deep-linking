f2edc94712468b50e3ff9c0bcb75b9879dc113c8
tristen <tristen.brown@gmail.com>
2015-01-21 17:27:33 -0500

    Make this not just about paragraphs

--- index.js
+++ index.js
@@ -5,84 +5,77 @@ var DeepLinking = function(el) {
 };
 
 DeepLinking.prototype = {
-  init: function(selector) {
-    this.paraSelctors = selector;
+  init: function(el) {
+    this.tagSelectors = [];
+
+    for (var i = 0; i < el.length; i++) {
+      if (this.tagInclusion(el[i].nodeName)) this.tagSelectors.push(el[i]);
+    }
 
     // Class names
     this.classActive = 'em-active';
     this.classInfo = 'em-info';
-    this.classSelectedParagraph = 'selected-paragraph';
-    this.classActiveParagraph = 'active-paragraph';
+    this.classSelectedTag = 'selected-tag';
+    this.classActiveTag = 'active-tag';
 
-    this.pl = false; // Paragraph List
-    this.p  = false; // Paragraph Anchor
-    this.h  = false; // Highlighted paragraphs
+    this.pl = false; // Tag List
+    this.p = false; // Tag Anchor
+    this.h = false; // Highlighted tags
 
     this.addCSS();
     this.readHash();
   },
 
+  tagInclusion: function(node) {
+    var pass = ['P', 'LI', 'BLOCKQUOTE', 'CODE', 'PRE', 'STRONG', 'EM', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+    return pass.indexOf(node) >= 0;
+  },
+
   addCSS: function() {
     var st = document.createElement('style');
     st.setAttribute('type', 'text/css');
-    var stStr = 'p.' + this.classSelectedParagraph + ' { display: block; position: relative; background: #F2F4F5; }' + '@-webkit-keyframes backgroundFade{ 0%{background:#FFF0B3;} 100%{background:transparent;}} p.' + this.classActiveParagraph + ' { display: block; position: relative; background-color: transparent; -webkit-animation:backgroundFade 5s; }';
+    var stStr = '.' + this.classSelectedTag + ' { display: block; position: relative; background: #F2F4F5; }' + '@-webkit-keyframes backgroundFade{ 0%{background:#FFF0B3;} 100%{background:transparent;}} .' + this.classActiveTag + ' { display: block; position: relative; background-color: transparent; -webkit-animation:backgroundFade 5s; }';
     try {
-      // try the sensible way
-      st.innerHTML = stStr;
+      st.innerHTML = stStr; // The sensible way
     } catch(e) {
-      // IE's way
-      st.styleSheet.cssText = stStr;
+      st.styleSheet.cssText = stStr; // IE's way
     }
     document.getElementsByTagName('head')[0].appendChild(st);
   },
 
   readHash: function() {
     // Read and interpret the URL hash
-    var lh = decodeURI(location.hash),
-        p  = false,
-        a, i, findp, findh, hi, key, pos;
+    var lh = decodeURI(location.hash);
+    var findp = lh.match(/\[([^[\]]*)\]/);
+    var p  = (findp && findp.length > 0) ? findp[1] : false;
 
-    findp = lh.match(/p\[([^[\]]*)\]/);
-    findh = lh.match(/h\[([^[\]]*)\]/);
-
-    p  = (findp && findp.length>0) ? findp[1] : false;
-    hi = (findh && findh.length>0) ? findh[1] : false;
-
-    if (hi) {
-      hi = hi.match(/[a-zA-Z]+(,[0-9]+)*/g);
-      for (i = 0; i < hi.length; i++) {
-        a   = hi[i].split(',');
-        key = a[0];
-        pos = this.findKey(key).index;
-      }
-    }
     this.p = p;
     this.goAnchor(p);
     this.highlightMatch();
   },
 
-  paragraphList: function() {
-    // Build a list of Paragrphs, keys, and add meta-data to each Paragraph in DOM, saves list for later re-use
+  tagList: function() {
+    // Build a list of Paragrphs, keys, and add meta-data to each Tag in DOM, saves list for later re-use
     if (this.pl && this.pl.list.length > 0) {
       return this.pl;
     }
-    var instance = this,
-        list = [],
-        keys = [],
-        c = 0,
-        len  = this.paraSelctors.length,
-        pr, k;
+    var instance = this;
+    var list = [];
+    var keys = [];
+    var c = 0;
 
-    for (var p = 0; p < len; p++) {
-      pr = this.paraSelctors[p];
+    for (var p = 0; p < this.tagSelectors.length; p++) {
+      var pr = this.tagSelectors[p];
       if ((pr.innerText || pr.textContent || '').length>0) {
-        k = instance.createKey(pr);
+        var k = instance.createKey(pr);
         list.push(pr);
         keys.push(k);
         pr.setAttribute('data-key', k); // Unique Key
         pr.setAttribute('data-num', c); // Order
         this.addEvent(pr, 'click', function(e) {
-          instance.paragraphClick(e);
+          if (e.currentTarget.nodeName && instance.tagInclusion(e.currentTarget.nodeName)) {
+            instance.tagClick(e);
+          }
         });
         c++;
       }
@@ -92,56 +85,37 @@ DeepLinking.prototype = {
     return this.pl;
   },
 
-  paragraphClick: function(e) {
+  tagClick: function(e) {
 
-    //Remove any active paragraph if there is one.
-    var activeEl = document.querySelector('p.' + this.classActiveParagraph);
-    if (activeEl) this.removeClass(activeEl, this.classActiveParagraph);
+    //Remove any active tag if there is one.
+    var activeEl = document.querySelector('.' + this.classActiveTag);
+    if (activeEl) this.removeClass(activeEl, this.classActiveTag);
 
     var change = false;
-    var pr = (e.currentTarget.nodeName === 'P') ? e.currentTarget : false; // Paragraph
-    var an = (e.target.nodeName === 'A')        ? e.target        : false; // Anchor
+    var target = e.currentTarget;
 
-    if (pr || an) {
-      if (!this.hasClass(pr, this.classSelectedParagraph)) {
-        this.updateParagraph(pr);
+    if (target) {
+      if (!this.hasClass(target, this.classSelectedTag)) {
+        this.updateTag(target);
         change = true;
       }
     }
     this.updateURLHash(change);
   },
 
-  paragraphInfo: function(mode) {
-    // Toggle anchor links next to Paragraphs
-    // Add an active link to paragraphs if one exists
-    var para, key, isActive, spans;
-    var hasSpan = document.querySelectorAll('span.' + this.classInfo);
-    if (hasSpan.length === 0) {
-      var pl  = this.paragraphList();
-      var len = pl.list.length;
-      for (var i = 0; i < len; i++) {
-        para = pl.list[i] || false;
-        if (para) {
-          key = pl.keys[i];
-          isActive = (key === this.p) ? para.className = this.classActiveParagraph : '';
-        }
-      }
-    }
-  },
-
-  updateParagraph: function(pr) {
+  updateTag: function(pr) {
     this.p = pr.getAttribute('data-key');
-    var selected = document.querySelector('p.' + this.classSelectedParagraph);
-    if (selected) this.removeClass(selected, this.classSelectedParagraph);
-    this.addClass(pr, this.classSelectedParagraph);
+    var selected = document.querySelector('.' + this.classSelectedTag);
+    if (selected) this.removeClass(selected, this.classSelectedTag);
+    this.addClass(pr, this.classSelectedTag);
   },
 
   updateURLHash: function(change) {
     var key, nSent, hash;
-    // Scan the Paragraphs, note selections, highlights and update the URL with the new Hash
+    // Scan the Tags, note selections, highlights and update the URL with the new Hash
     if (change) {
       var h = 'h[',
-          paras = document.querySelectorAll('p.emReady'),
+          paras = document.querySelectorAll('.emReady'),
           pLen  = paras.length;
 
       for (var p = 0; p < pLen; p++) {
@@ -153,18 +127,18 @@ DeepLinking.prototype = {
         }
       }
 
-      var anchor = ((this.p) ? 'p[' + this.p + '],' : '');
+      var anchor = ((this.p) ? '[' + this.p + '],' : '');
       hash = (anchor + (h.replace('h[,', 'h[') + ']')).replace(',h[]', '');
     } else {
-      var selected = document.querySelector('p.' + this.classSelectedParagraph);
-      this.removeClass(selected, this.classSelectedParagraph);
+      var selected = document.querySelector('.' + this.classSelectedTag);
+      this.removeClass(selected, this.classSelectedTag);
       hash = '_';
     }
     location.hash = hash;
   },
 
   createKey: function(p) {
-    // From a Paragraph, generate a Key
+    // From a Tag, generate a Key
     var key = '',
         len = 6,
         txt = (p.innerText || p.textContent || '').replace(/[^a-z\. ]+/gi, ''),
@@ -187,8 +161,8 @@ DeepLinking.prototype = {
   },
 
   findKey: function(key) {
-    // From a list of Keys, locate the Key and corresponding Paragraph
-    var pl = this.paragraphList(),
+    // From a list of Keys, locate the Key and corresponding Tag
+    var pl = this.tagList(),
         ln = pl.keys.length,
         ix = false,
         el = false,
@@ -212,9 +186,9 @@ DeepLinking.prototype = {
   },
 
   goAnchor: function(p) {
-    // Move view to top of a given Paragraph
+    // Move view to top of a given Tag
     if (!p) return;
-    var pg = (isNaN(p)) ? this.findKey(p).elm : (this.paragraphList().list[p-1] || false);
+    var pg = (isNaN(p)) ? this.findKey(p).elm : (this.tagList().list[p-1] || false);
     var self = this;
 
     if (pg) {
@@ -228,20 +202,20 @@ DeepLinking.prototype = {
     var para, key, isActive;
     var hasSpan = document.querySelectorAll('span.' + this.classInfo);
     if (hasSpan.length === 0) {
-      var pl  = this.paragraphList();
+      var pl  = this.tagList();
       var len = pl.list.length;
       for (var i = 0; i < len; i++) {
         para = pl.list[i] || false;
         if (para) {
           key = pl.keys[i];
-          isActive = (key === this.p) ? para.className = this.classActiveParagraph : '';
+          isActive = (key === this.p) ? para.className = this.classActiveTag : '';
         }
       }
     }
   },
 
   getSentences: function(el) {
-    // Break a Paragraph into Sentences, bearing in mind that the "." is not the definitive way to do so
+    // Break a tag into Sentences, bearing in mind that the "." is not the definitive way to do so
     var html    = (typeof el === 'string') ? el : el.innerHTML,
         mrsList = 'Mr,Ms,Mrs,Miss,Msr,Dr,Gov,Pres,Sen,Prof,Gen,Rep,St,Messrs,Col,Sr,Jf,Ph,Sgt,Mgr,Fr,Rev,No,Jr,Snr',
         topList = 'A,B,C,D,E,F,G,H,I,J,K,L,M,m,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,etc,oz,cf,viz,sc,ca,Ave,St',
@@ -275,13 +249,6 @@ DeepLinking.prototype = {
     return lines;
   },
 
-  ordinal: function(n) {
-    // http://en.wikipedia.org/wiki/Ordinal_number
-    var sfx = ['th','st','nd','rd'],
-        val = n%100;
-    return n + (sfx[(val-20)%10] || sfx[val] || sfx[0]);
-  },
-
   lev: function(a, b) {
     // Get the Levenshtein distance - a measure of difference between two sequences
     // Based on http://andrew.hedges.name/experiments/levenshtein/levenshtein.js
@@ -309,11 +276,6 @@ DeepLinking.prototype = {
     if (x < y && x < z) { return x; }
     if (y < x && y < z) { return y; }
     return z;
-  },
-
-  rtrim: function(txt) {
-    // Trim whitespace from right of string
-    return txt.replace(/\s+$/, '');
   },
 
   cleanArray: function(a){
